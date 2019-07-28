86663c281d91500d1bc07eea9e677e55ebdd062a
Diego <dhurtad2@uci.edu>
2019-07-28 16:24:32 -0700

    added origin and class legend for the champion class

--- tft_project/unit.py
+++ tft_project/unit.py
@@ -1,8 +1,39 @@
+'''
+ORIGIN LEGEND
 
+1 DEMON
+2 DRAGON
+3 EXILE
+4 GLACIAL
+5 IMPERIAL
+6 NINJA
+7 NOBLE
+8 PHANTOM
+9 PIRATE
+10 ROBOT
+11 VOID
+12 WILD
+13 YORDLE
+
+
+CLASS LEGEND
+
+1 ASSASSIN
+2 BLADEMASTER
+3 BRAWLER
+4 ELEMENTALIST
+5 GUARDIAN
+6 GUNSLINGER
+7 KNIGHT
+8 RANGER
+9 SHAPESHIFTER
+10 SORCERER
+
+'''
 
 class champion:
 
-    def __init__(self, blasses: [str], origin; str):
+    def __init__(self, blasses: [int], origin: int):
         self.blass = blasses
         self.origin = origin
 
