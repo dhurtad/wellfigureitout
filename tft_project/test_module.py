a60ad40f541c0318536daa806eaa35cba3a33031
Diego <dhurtad2@uci.edu>
2019-07-28 16:51:00 -0700

    new tests for champion class methods

--- tft_project/test_module.py
+++ tft_project/test_module.py
@@ -3,7 +3,24 @@ import unit
 import main
 
 class TestingModule(unittest.TestCase):
-    pass
+    
+    #Testing unit module
+    def test_unit_name(self):
+        test_champ = unit.champion('name', [1], 1, 1)
+        test_champ.get_name() == 'name'
+
+    def test_unit_origin(self):
+        test_champ = unit.champion('name', [1], 1, 1)
+        test_champ.get_origin() == 1
+
+    def test_unit_class(self):
+        test_champ = unit.champion('name', [1], 1, 1)
+        test_champ.get_blass() ==[1]
+
+    def test_unit_cost(self):
+        test_champ = unit.champion('name', [1], 1, 1)
+        test_champ.get_cost() == 1
+        
 
 if __name__ == '__main__':
     unittest.main()
