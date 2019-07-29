aed8e8586a96d34ba6bd09b289b56fdc126e9a93
Diego <dhurtad2@uci.edu>
2019-07-28 17:19:53 -0700

    added get_champ_dict function that downloads the unit data and returns it as a dictionary from the new api

--- tft_project/main.py
+++ tft_project/main.py
@@ -1,7 +1,24 @@
-import unit
 
-units = []
+import urllib.request
+import json
+
+def get_champ_dict() -> dict:
+    '''
+    Downloads champion unit data from an api found online and
+    returns a dictionary of champion units with their individual
+    data. 
+    '''
+    url = "https://solomid-resources.s3.amazonaws.com/blitz/tft/data/champions.json"
+    response = urllib.request.urlopen(url)
+    data = response.read()
+    response.close()
+    text = data.decode(encoding = 'utf-8')
+
+    return json.loads(text)
 
 if __name__ == '__main__':
+    champs = get_champ_dict()
+    
+
     
     
