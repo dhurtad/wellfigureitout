329cb1e23f50fe00fc8c2a0cf1c5ed184590d806
Diego <dhurtad2@uci.edu>
2019-07-29 00:39:52 -0700

    emptied the tests for the unit.py module that got deleted

--- tft_project/test_module.py
+++ tft_project/test_module.py
@@ -1,25 +1,8 @@
 import unittest
-import unit
 import main
 
 class TestingModule(unittest.TestCase):
-    
-    #Testing unit module
-    def test_unit_name(self):
-        test_champ = unit.champion('name', [1], 1, 1)
-        test_champ.get_name() == 'name'
-
-    def test_unit_origin(self):
-        test_champ = unit.champion('name', [1], 1, 1)
-        test_champ.get_origin() == 1
-
-    def test_unit_class(self):
-        test_champ = unit.champion('name', [1], 1, 1)
-        test_champ.get_blass() ==[1]
-
-    def test_unit_cost(self):
-        test_champ = unit.champion('name', [1], 1, 1)
-        test_champ.get_cost() == 1
+    pass
         
 
 if __name__ == '__main__':
