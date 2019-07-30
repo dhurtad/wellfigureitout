041633209b4607bd4b95f3c5f6b5e919598013a4
jonestrada7 <49575738+jonestrada7@users.noreply.github.com>
2019-07-29 21:16:45 -0700

    Moved main.py script to interface.py
    High-cohesion, low-coupling :P

---
+++ tft_project/interface.py
@@ -0,0 +1,36 @@
+# Runs main event loop
+
+import main
+import prompt
+
+# Static APIs
+
+CHAMP_API = "https://solomid-resources.s3.amazonaws.com/blitz/tft/data/champions.json"
+CLASSES_API = "https://solomid-resources.s3.amazonaws.com/blitz/tft/data/classes.json"
+ORIGINS_API = "https://solomid-resources.s3.amazonaws.com/blitz/tft/data/origins.json"
+
+if __name__ == '__main__':
+    # Execute necessary functions here
+    
+    champs = main.get_tft_dict(CHAMP_API)
+    class_slots = main.get_slots(main.get_tft_dict(CLASSES_API))
+    origin_slots = main.get_slots(main.get_tft_dict(ORIGINS_API))
+    print(class_slots)
+    print(origin_slots)
+    while True:
+        level = prompt.for_int('Input your current level')
+        comp = []
+
+        for x in range(level):
+            while True:
+                champ = prompt.for_string('Input champ #' + str(x+1))
+                if champ.upper() in [x.upper() for x in champs.keys()]:
+                    comp.append(champ)
+                    break
+
+                else:
+                    print('This is not a valid unit name')
+
+        origins, classes = main.find_comp_origins_classes(comp)
+        origins = main.count_dict(origins)
+        classes = main.count_dict(classes)
