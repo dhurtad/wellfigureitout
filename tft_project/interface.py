2cd334d2600b7cb70131e67fb52fa721659aa1f6
jonestrada7 <49575738+jonestrada7@users.noreply.github.com>
2019-07-30 23:21:23 -0700

    Now returns options for future comp builds
    Imports Champ_Data object, returns possible champ options depending on which origin/class player would like to build off of

--- tft_project/interface.py
+++ tft_project/interface.py
@@ -2,11 +2,13 @@
 
 import main
 import prompt
+from champ_data import Champ_Data
 
 if __name__ == '__main__':
     # Execute necessary functions here
     game_status = True
     champs = main.get_tft_dict(main.CHAMP_API)
+    # print(champs)
     class_slots = main.get_slots(main.get_tft_dict(main.CLASSES_API))
     origin_slots = main.get_slots(main.get_tft_dict(main.ORIGINS_API))
     print(class_slots)
@@ -30,8 +32,28 @@ if __name__ == '__main__':
         origins = main.count_dict(origins)
         classes = main.count_dict(classes)
         
-        print(origins)
-        print(classes)
+        print('Your current origins are: ')
+        print(dict(origins))
+        print('Your current classes are: ')
+        print(dict(classes))
+        
+        # Returning relevant information/data to player
+        
+        champ_data = Champ_Data(dict(origins), dict(classes))
+        choice = prompt.for_int('Would you like to build your comp off of an origin (0) or class (1)')
+        while True:
+            try:
+                if choice == 0:
+                    origin = prompt.for_string('Which origin would you like to build off of? Please input an origin')
+                    print('Your origin options are: ', champ_data.get_origin_options(origin))
+                    break
+                else:
+                    blass = prompt.for_string('Which origin would you like to build off of? Please input an origin')
+                    print('Your class options are: ', champ_data.get_class_options(blass))
+                    break
+            except NameError:
+                print('Invalid origin/class')
+            
         
         # Ask if game has ended or not (Only for console version; this is temporary)
         game_status_input = prompt.for_string('Has the game ended? (Y or N)')
